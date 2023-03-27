55233b99904f47bef6c7c54bfddd2fb2a984fae5
Aadinath1234 <93751167+Aadinath1234@users.noreply.github.com>
2023-03-27 08:29:57 +0530

    this files belongs to src folder

---
+++ pizzasdata.js
@@ -0,0 +1,93 @@
+const pizzas = [  {    name: "PEPPER BARBECUE CHICKEN",    varients: ["small", "medium", "large"],
+    prices: [
+      {
+        small: 200,
+        medium: 350,
+        large: 400,
+      },
+    ],
+    category: "nonveg",
+    image: "https://www.dominos.co.in/files/items/Pepper_Barbeque.jpg",
+    description: "Pepper Barbecue Chicken Cheese",
+  },
+  {
+    name: "Non veg Supreme",
+    varients: ["small", "medium", "large"],
+    prices: [
+      {
+        small: 180,
+        medium: 250,
+        large: 350,
+      },
+    ],
+    category: "Non-veg",
+    image:
+      "https://www.bravabod.com/wp-content/uploads/2021/01/SupremeMeatzaPizza_c.jpg",
+    description:
+      "Corn over the base makes it look beautiful . It is served with tomato-sauce and chilli flakes are spread over it",
+  },
+  {
+    name: "Golden Corn Pizza",
+    varients: ["small", "medium", "large"],
+    prices: [
+      {
+        small: 180,
+        medium: 250,
+        large: 350,
+      },
+    ],
+    category: "veg",
+    image: "https://www.crazymasalafood.com/wp-content/images/golden-1.jpg",
+    description:
+      "Corn over the base makes it look beautiful . It is served with tomato-sauce and chilli flakes are spread over it",
+  },
+  {
+    name: "Jalapeno & Red Paprika Pizza",
+    varients: ["small", "medium", "large"],
+    prices: [
+      {
+        small: 200,
+        medium: 300,
+        large: 420,
+      },
+    ],
+    category: "veg",
+    image:
+      "https://coloradough.pizza/wp-content/uploads/2020/09/jalapeno-popper.jpg",
+    description:
+      "This pizza is amazing and can become more delicious if we will add some more cheese in it.",
+  },
+  {
+    name: "Margerita",
+    varients: ["small", "medium", "large"],
+    prices: [
+      {
+        small: 150,
+        medium: 220,
+        large: 300,
+      },
+    ],
+    category: "veg",
+    image:
+      "https://media.gettyimages.com/id/157741539/photo/pizza-margherita.jpg?s=2048x2048&w=gi&k=20&c=ykB6C-edSGrWe2QcCNNwgRYhMsHV7pdwDvq1UM5_ZMc=",
+    description:
+      "The pizza base is made by mixing yeast, sugar, olive oil, salt and all-purpose flour in a big bowl",
+  },
+  {
+    name: "Double Cheese Margerita Pizza",
+    varients: ["small", "medium", "large"],
+    prices: [
+      {
+        small: 250,
+        medium: 380,
+        large: 450,
+      },
+    ],
+    category: "veg",
+    image: "https://www.crazymasalafood.com/wp-content/images/double-1.jpg",
+    description:
+      "This is a plain pizza which has cheese on it which is margherita and is delicious because of the flavor",
+  },
+];
+
+export default pizzas;
